5198829de3c636a813a2211b99d3564926ccd853
kennethraphaelkeil <kennethraphaelkeil@gmail.com>
2023-01-22 20:17:13 +0100

    first commit

---
+++ src/components/cards/VerticalProduct.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>VerticalProduct</div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object,
+    addToCart: Function,
+    i: Number
+  }
+}
+</script>
---
+++ src/components/home/PopularProducts.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="fill-height mb-12">
+    <v-row>
+      <v-col sm="10" offset-sm="1" lg="8" offset-lg="2">
+        <h2>Popular Products <v-btn to="store" text small color="primary">View All</v-btn></h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col sm="10" offset-sm="1" lg="8" offset-lg="2">
+        <v-row>
+          <v-col
+            sm="6"
+            md="4"
+            v-for="(product) in $store.state.products.slice(0, 3)"
+            :key="product.name"
+          >
+            <v-card outlined>
+              <v-card-tite>{{ product.name }}</v-card-tite>
+              <v-card-subtitle>${{ product.price }}</v-card-subtitle>
+              <v-card-actions>
+                <v-btn color="success" outlined>
+                  Add to Cart
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// import VerticalProduct from '@/components/cards/VerticalProduct.vue'
+
+export default {
+  components: {
+    // VerticalProduct
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    addToCart(index, quantity = 1) {
+      this.addItemToCart({ itemId: index, quantity })
+      this.updateSnackbar({ show: true })
+    }
+  }
+}
+</script>
